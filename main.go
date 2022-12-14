f126871778dc6a99358d7cfc1eb1378072aa961b
Varrel Marcellius <varrel.marcellius@springkraf.com>
2022-12-15 02:01:09 +0700

    feat: update db config and run seeder

--- main.go
+++ main.go
@@ -19,16 +19,16 @@ func init() {
 
 func handleArgs() {
 	flag.Parse()
-	args := flag.Args()
+	//args := flag.Args()
 
-	if len(args) >= 1 {
-		switch args[0] {
-		case "seeder":
-			userRepository := repositories.NewUserRepository(database.GetDB())
-			userSeed := seed.NewUserSeeder(userRepository)
-			userSeed.Execute()
-		}
-	}
+	//if len(args) >= 1 {
+	//	switch args[0] {
+	//	case "seeder":
+	userRepository := repositories.NewUserRepository(database.GetDB())
+	userSeed := seed.NewUserSeeder(userRepository)
+	userSeed.Execute()
+	//}
+	//}
 }
 
 func main() {
